51dd4009fa90ec940c7904ad53e5cd348526f5ea
Cristhian <csotelo88@hotmail.com>
2019-11-12 18:40:28 -0700

    Update A3Tests.java

--- A3Tests.java
+++ A3Tests.java
@@ -12,7 +12,13 @@ import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import myClasses.Airplane;
+
 public class A3Tests {
+  private ByteArrayOutputStream testStream;
+  private final PrintStream originalPrint = System.out;
 
   @Test
   public void testPParser() {
@@ -20,6 +26,34 @@ public class A3Tests {
     assertEquals("\ntestPParser boolean", true, PParser.parse(boolean.class, "true"));
     assertEquals("\ntestPParser char", 'C', PParser.parse(char.class, "C"));
     assertEquals("\ntestPParser String", "String", PParser.parse(String.class, "String"));
+  }
+
+  @Test
+  public void testInspector() {
+    testStream = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(testStream));
+    Inspector inspector = new Inspector();
+    Airplane airP = new Airplane(1234, 40, true);
+    inspector.inspect(airP, true);
+    assertEquals("\nAirplane class inspection", "CLASS: myClasses.Airplane\n"
+      +"  FIELDS: FROM: myClasses.Airplane\n"
+      +"    FIELD NAME: id FROM myClasses.Airplane\n"
+      +"    TYPE: int\n"
+      +"    MODIFIER: private\n"
+      +"    VALUE: 1234\n"
+      +"    -----------------\n"
+      +"    FIELD NAME: seats FROM myClasses.Airplane\n"
+      +"    TYPE: int\n"
+      +"    MODIFIER: private\n"
+      +"    VALUE: 40\n"
+      +"    -----------------\n"
+      +"    FIELD NAME: active FROM myClasses.Airplane\n"
+      +"    TYPE: boolean\n"
+      +"    MODIFIER: private\n"
+      +"    VALUE: true\n"
+      +"    -----------------\n"
+      +"======END OF CLASS myClasses.Airplane======\n", testStream.toString());
+    System.setOut(originalPrint);
   }
 
   public static void main( String[] args ) {
